cf8f21033bd4c6bb5f06c6984d138270ed57c4cb
Zev Stravitz <zevstravitz2021@u.northwestern.edu>
2019-02-24 16:26:38 -0600

    added gamemaster and search algorithms, no passes

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -6,7 +6,7 @@ class TowerOfHanoiGame(GameMaster):
 
     def __init__(self):
         super().__init__()
-        
+
     def produceMovableQuery(self):
         """
         See overridden parent class method for more information.
@@ -34,28 +34,35 @@ class TowerOfHanoiGame(GameMaster):
             A Tuple of Tuples that represent the game state
         """
 
-        find_disk = 0
-        while (True):
-            found = 0
-            peg1_tuple, peg2_tuple, peg3_tuple = (),(),()
-            for fact in GameMaster.kb.facts:
-                if fact.statement.predicate == 'on':
-                    disk_string = fact.statement.terms[0].term.element
-                    peg_string = fact.statement.terms[1].term.element
-                    diskNumber = int(disk_string[-1])
+        disks_on_peg1 = self.kb.kb_ask(parse_input('fact: (on ?disk peg1'))
+        disks_on_peg2 = self.kb.kb_ask(parse_input('fact: (on ?disk peg2'))
+        disks_on_peg3 = self.kb.kb_ask(parse_input('fact: (on ?disk peg3'))
+        t1 = []
+        t2 = []
+        t3 = []
 
-                    if diskNumber == find_disk:
-                        found = 1
-                        if peg_string == "peg1":
-                            peg1_tuple += ([diskNumber])
-                        elif peg_string == "peg2":
-                            peg2_tuple += ([diskNumber])
-                        elif peg_string == "peg3":
-                            peg3_tuple += ([diskNumber])
-                        find_disk += 1
-                        break
-            if found == 0:
-                return (peg1_tuple, peg2_tuple,peg3_tuple)
+        if disks_on_peg1:
+            for disks in disks_on_peg1:
+                # get the name and take the last index for the number
+                disk = int(str(disks.bindings[0].constant)[-1])
+                t1.append(disk)
+        if disks_on_peg2:
+            for disks in disks_on_peg2:
+                disk = int(str(disks.bindings[0].constant)[-1])
+                t2.append(disk)
+        if disks_on_peg3:
+            for disks in disks_on_peg3:
+                disk_string = str(disks.bindings[0].constant)[-1]
+                disk_int = int(disk_string)
+                t3.append(disk_int)
+
+        t1.sort()
+        t2.sort()
+        t3.sort()
+
+        gs_hanoi = (tuple(t1), tuple(t2), tuple(t3))
+
+        return gs_hanoi
 
     def makeMove(self, movable_statement):
         """
@@ -74,10 +81,37 @@ class TowerOfHanoiGame(GameMaster):
             None
         """
         move_disk = movable_statement.terms[0].term.element
-        peg1 = movable_statement.terms[1].term.element
-        peg2 = movable_statement.terms[2].term.element
+        peg_init = movable_statement.terms[1].term.element
+        peg_final = movable_statement.terms[2].term.element
 
+        # ______________RESET FACTS AT OLD POSITION_______________
+        self.kb.kb_retract(parse_input("fact: (on " + move_disk + " " + peg_init + ")"))
+        self.kb.kb_retract(parse_input("fact: (top " + move_disk + " " + peg_init + ")"))
 
+        old_under_disk = self.kb.kb_ask(parse_input("fact: (onTopOf " + move_disk + "?x)"))
+
+        if not old_under_disk:
+            self.kb.kb_assert(parse_input('fact: (empty ' + peg_init + ')'))
+        else:
+            below_disk = old_under_disk[0].bindings_dict['?x']
+            self.kb.kb_assert(parse_input('fact: (top ' + below_disk + ' ' + peg_init + ')'))
+            self.kb.kb_retract(parse_input('fact: (onTopOf ' + move_disk + ' ' + below_disk + ')'))
+
+        # ______________RESET FACTS AT NEW POSITION_______________
+        new_empty_under = self.kb.kb_ask(parse_input('fact: (empty ' + peg_final + ')'))
+
+        if not new_empty_under:
+            curr_target_top = self.kb.kb_ask(parse_input('fact: (top ?x ' + peg_final + ')'))
+            below_disk = curr_target_top[0].bindings_dict['?x']
+            self.kb.kb_retract(parse_input('fact: (top ' + below_disk + ' ' + peg_final + ')'))
+
+            self.kb.kb_assert(parse_input('fact: (top ' + move_disk + ' ' + peg_final))
+            self.kb.kb_assert(parse_input('fact: (onTopOf ' + move_disk + ' ' + below_disk))
+            self.kb.kb_assert(parse_input('fact: (on ' + move_disk + ' ' + peg_final + ')'))
+        else:
+            self.kb.kb_retract(parse_input('fact: (empty ' + peg_final + ')'))
+            self.kb.kb_assert(parse_input('fact: (top ' + move_disk + ' ' + peg_final + ')'))
+            self.kb.kb_assert(parse_input('fact: (on ' + move_disk + ' ' + peg_final + ')'))
 
     def reverseMove(self, movable_statement):
         """
@@ -123,21 +157,28 @@ class Puzzle8Game(GameMaster):
         Returns:
             A Tuple of Tuples that represent the game state
         """
+
+
+        #all rows begin as being empty
         row1, row2, row3 = (),(),()
+        # comb through all of the tiles
         for column in range(1,5):
             for row in range(1,5):
+                # assume tile hasn't been found
                 found = 0
                 for fact in self.kb.facts:
                     if fact.statement.predicate == "posn":
+                        # Break up the fact
                         tile_string = fact.statement.terms[0].term.element
                         column_string = fact.statement.terms[1].term.element
                         row_string = fact.statement.terms[2].term.element
 
+                        # Convert to integer form
                         tile_num = int(tile_string[-1])
                         column_num = int(column_string[-1])
                         row_num = int(row_string[-1])
 
-                        if row_num == row and column_num == column:
+                        if row_num == row and column_num == column: # If the loop position matches the pos from the Fact
                             found = 1
                             if row_num == 1:
                                 row1 += tuple([tile_num])
@@ -147,6 +188,7 @@ class Puzzle8Game(GameMaster):
                                 row3 += tuple([tile_num])
                             break
 
+                # Where there is no tile to be found at a position, we assume it's empty...
                 if found == 0:
                     if row == 1:
                         row1 += tuple([-1])
@@ -173,8 +215,23 @@ class Puzzle8Game(GameMaster):
         Returns:
             None
         """
-        ### Student code goes here
-        pass
+        tile = movable_statement.terms[0].term.element
+        col1 = movable_statement.terms[1].term.element
+        row1 = movable_statement.terms[2].term.element
+        col2 = movable_statement.terms[3].term.element
+        row2 = movable_statement.terms[4].term.element
+
+        fact_old_posn = parse_input("fact: (coordinate " + tile + " " + col1 + " " + row1 + ")")
+        self.kb.kb_retract(fact_old_posn)
+
+        fact_new_posn = parse_input("fact: (coordinate " + tile + " " + col2 + " " + row2 + ")")
+        self.kb.kb_assert(fact_new_posn)
+
+        fact__old_empty_posn = parse_input("fact: (coordinate empty " + col2 + " " + row2 + ")")
+        self.kb.kb_retract(fact__old_empty_posn)
+
+        fact__new_empty_posn = parse_input("fact: (coordinate empty " + col1 + " " + row1 + ")")
+        self.kb.kb_assert(fact__new_empty_posn)
 
     def reverseMove(self, movable_statement):
         """
--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -1,6 +1,19 @@
 
 from solver import *
 
+class Queue:
+    def __init__(self, data=[]):
+        self.visited = data[:]
+
+    def isempty(self):
+        return len(self.visited) == 0
+
+    def enqueue(self, x):
+        return self.visited.append(x)
+
+    def dequeue(self):
+        return self.visited.pop(0)
+
 class SolverDFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
         super().__init__(gameMaster, victoryCondition)
@@ -18,13 +31,57 @@ class SolverDFS(UninformedSolver):
         Returns:
             True if the desired solution state is reached, False otherwise
         """
-        ### Student code goes here
-        return True
 
+        # first, check to see if the current gamestate is solved
+        if self.gm.getGameState() == self.victoryCondition:
+            return True
+
+        frontier = self.gm.getMovables()
+
+        if len(frontier) == 0:
+            self.currentState = self.currentState.parent
+            self.solveOneStep()
+        else:
+            find_unvisited = False
+            for move in frontier:
+                self.gm.makeMove(move)
+                child = GameState(self.gm.getGameState(), self.currentState.depth + 1, move)
+                self.gm.reverseMove(move)
+                if child in self.visited:
+                    continue
+                find_unvisited = True
+                self.currentState.children.append(child)
+                child.parent = self.currentState
+                self.visited[child] = True
+                self.gm.makeMove(move)
+                self.currentState = child
+                break
+            if not find_unvisited:
+                self.gm.reverseMove(move)
+                self.currentState = self.currentState.parent
+                self.solveOneStep()
 
 class SolverBFS(UninformedSolver):
+
+
     def __init__(self, gameMaster, victoryCondition):
         super().__init__(gameMaster, victoryCondition)
+        self.explored = Queue()
+
+    def findNextNode(self, nextGameState):
+        while not self.currentState.parent:
+            self.gm.reverseMove(self.currentState.requiredMovable)
+            self.currentState = self.currentState.parent
+        while nextGameState.depth != self.currentState.depth:
+            traverse_node = nextGameState
+            while traverse_node.depth != (self.currentState.depth + 1):
+                traverse_node = traverse_node.parent
+            for child in self.currentState.children:
+                if child.requiredMovable == traverse_node.requiredMovable:
+                    self.gm.makeMove(child.requiredMovable)
+                    self.currentState = child
+                    break
+        return
 
     def solveOneStep(self):
         """
@@ -39,5 +96,39 @@ class SolverBFS(UninformedSolver):
         Returns:
             True if the desired solution state is reached, False otherwise
         """
-        ### Student code goes here
-        return True
+        if self.gm.getGameState() == self.victoryCondition:
+            return True
+
+        if self.explored.isempty():
+            frontier = self.gm.getMovables()
+            for move in frontier:
+                self.gm.makeMove(move)
+                curr_gs = GameState(self.gm.getGameState(), self.currentState.depth + 1, move)
+
+                if not self.visited.get(curr_gs, False):
+                    if not curr_gs in self.currentState.children:
+                        self.currentState.children.append(curr_gs)
+                        curr_gs.parent = self.currentState
+                        self.visited[curr_gs] = True
+                        self.explored.enqueue(curr_gs)
+
+                self.gm.reverseMove(move)
+
+        next_gs = self.explored.dequeue()
+        self.findNextNode(next_gs)
+        if self.gm.getGameState() == self.victoryCondition:
+            return True
+
+        frontier = self.gm.getMovables()
+        for move in frontier:
+            self.gm.makeMove(move)
+            curr_gs = GameState(self.gm.getGameState(), self.currentState.depth + 1, move)
+
+            if not self.visited.get(curr_gs, False):
+                if not curr_gs in self.currentState.children:
+                    self.currentState.children.append(curr_gs)
+                    curr_gs.parent = self.currentState
+
+                    self.visited[curr_gs] = True
+                    self.explored.enqueue(curr_gs)
+            self.gm.reverseMove(move)
